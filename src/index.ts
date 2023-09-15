e8a8ceadbbb5ecd012fa36126166e44e672f8c5f
Nayan <nayanyadav219@gmail.com>
2023-09-15 22:23:10 +0530

    changed port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
